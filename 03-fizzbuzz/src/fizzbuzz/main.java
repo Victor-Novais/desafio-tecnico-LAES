7e717842a0dc03034694297bdeb4298f4f36828d
Thiago Lima <thiagobenedictis@gmail.com>
2025-08-26 18:33:35 -0300

    feat: Resolve o desafio '[3. FizzBuzz]'

---
+++ 03-fizzbuzz/src/fizzbuzz/main.java
@@ -0,0 +1,12 @@
+package fizzbuzz;
+
+import java.util.Arrays;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(Arrays.toString(FizzBuzz.fizzCheck(60)));
+	}
+
+}
