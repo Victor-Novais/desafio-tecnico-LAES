e1469c34da16d59489f049a9be323d301b4d0d0a
Thiago Lima <thiagobenedictis@gmail.com>
2025-08-26 18:37:55 -0300

    feat: Resolve o desafio '[9. Missing Number]'

---
+++ 09-missing-number/src/missingNumber/MissingNumber.java
@@ -0,0 +1,15 @@
+package missingNumber;
+
+public class MissingNumber {
+	
+	public static int encontrar(int[] nums) {
+		 int count[] = new int[nums.length+1]; 
+		 for(int i=0;i<nums.length;i++){
+		 count[nums[i]]++;
+		 }
+		 for(int i=0;i<=nums.length;i++){
+		 if(count[i]==0) return i;
+		 }
+		 return -1;
+		 }
+}
