9b8657f642ef09e7a2d05685de74f0e704f54220
Thiago Lima <thiagobenedictis@gmail.com>
2025-08-26 18:34:21 -0300

    feat: Resolve o desafio '[4. Maximum Subarray (Subarray com Maior Soma)]'

---
+++ 04-maximum-subarray/src/maximumSubArray/Main.java
@@ -0,0 +1,10 @@
+package maximumSubArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print(MaximumSubArray.maximumSum(new int[]{1,2,1,2,-5, 4, -10, 5,}));
+	}
+
+}
